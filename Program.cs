a5bd43a28fd738edafb008a505e0bcf3850f8ebb
qcraven01 <qcraven01@gmail.com>
2019-02-15 18:38:36 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Dog
+{
+    public class Dog
+
+    //I got stuck on adding gender into my program
+    {
+        public string name;
+        public string owner;
+        public int age;
+      
+
+        public Dog(string name, string owner, int age)
+        {
+            this.name = name;
+            this.owner = owner;
+            this.age = age;
+            //I am stuck here... I cant figure out how to add this.gender = gender; It's diff from the other three above because it is type enum
+        }
+        public void Bark(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+
+
+                Console.WriteLine("Woof");
+            }
+           
+        }
+        public string GetTag()
+        {
+            return String.Format("If lost, call {0}. His name is {1} and he is {2} years old.", owner, name, age);
+        }
+
+        class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Dog puppy = new Dog("Onion", "Bill", 1);
+            puppy.Bark(3);
+            Console.WriteLine(puppy.GetTag());
+        }
+    }
+    
+            
+        
+        }   
+
+
+        }
+
+    
+
